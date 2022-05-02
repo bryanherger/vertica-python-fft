0a541010b0c45f68a872b617ee6809e9f37b457e
bryanherger <bryanherger@gmail.com>
2022-05-01 22:26:25 -0400

    Update to v11
    Describe how to include Python packages using DEPENDS clause rather than modifying server install.  Also fix object name to be more unique and less likely to collide with other examples.

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ A Python UDTF for Vertica that implements Numpy Fast Fourier Transform (FFT)
 
 What's included: the Python library and a sample SQL to load and test the library.
 
-What's NOT included: Vertica only ships with a basic set of site packages, not including numpy.  You can install numpy into Vertica's Python environment by running the following command on all nodes:
+What's NOT included: Vertica only ships with a basic set of site packages, not including numpy.  You can install numpy into Vertica's Python environment by installing to a virtualenv and uploading that using the DEPENDS clause:
 
-sudo /opt/vertica/oss/python3/bin/python3 -m pip install numpy
+```
+/opt/vertica/oss/python3/bin/python3 -m venv ${VENV_PATH}
+source ${VENV_PATH}/bin/activate
+pip3 install numpy
+python3 -c "import sys;print(sys.path);"
+# The sys.path you want is likely last as ${VENV_PATH}/lib/python3.9/site-packages. Put this in DEPENDS in the sql.
+```
 
 For more info and an example usage, please see the blog post at https://www.vertica.com/blog/extending-vertica-with-python-functions-adding-numpy-fft-as-a-udx/
--- pyFFT.sql
+++ pyFFT.sql
@@ -1,36 +1,38 @@
 -- Step 1: Create library
--- be sure to install numpy into all nodes of your Vertica cluster as described in the README
-\set libfile '\'/home/bryan/udx/pyFFT.py\''
-CREATE LIBRARY TransformFunctions AS :libfile LANGUAGE 'Python';
+CREATE LIBRARY fftTransformFunctions AS '/tmp/pyFFT.py' DEPENDS '/path/to/lib/python3.9/site-packages' LANGUAGE 'Python';
 
 -- Step 2: Create functions
-CREATE TRANSFORM FUNCTION pyFFT AS NAME 'pyFFTFactory' LIBRARY TransformFunctions;
+CREATE TRANSFORM FUNCTION pyFFT AS NAME 'pyFFTFactory' LIBRARY fftTransformFunctions;
 
 -- Step 3: Use Functions
 -- create a data set of amplitude vs. time
-CREATE TABLE t (a FLOAT, b FLOAT);
-INSERT INTO t VALUES (0,0);
-INSERT INTO t VALUES (0.001,0.141120008);
-INSERT INTO t VALUES (0.002,-0.279415498);
-INSERT INTO t VALUES (0.003,0.412118485);
-INSERT INTO t VALUES (0.004,-0.536572918);
-INSERT INTO t VALUES (0.005,0.65028784);
-INSERT INTO t VALUES (0.006,-0.750987247);
-INSERT INTO t VALUES (0.007,0.836655639);
-INSERT INTO t VALUES (0.008,-0.905578362);
-INSERT INTO t VALUES (0.009,0.956375928);
-INSERT INTO t SELECT a+0.01, 0.0 FROM t;
-INSERT INTO t SELECT a+0.02, 0.0 FROM t;
-INSERT INTO t SELECT a+0.04, 0.0 FROM t;
-INSERT INTO t SELECT a+0.08, 0.0 FROM t;
-INSERT INTO t SELECT a+0.16, 0.0 FROM t;
-INSERT INTO t SELECT a+0.32, 0.0 FROM t;
-INSERT INTO t SELECT a+0.64, 0.0 FROM t;
-UPDATE t SET b = sin(40 * 2 * 3.14 * a) + 0.5 * sin(90 * 2 * 3.14 * a);
-SELECT * FROM t;
+CREATE TABLE t_fft (a FLOAT, b FLOAT);
+INSERT INTO t_fft VALUES (0,0);
+INSERT INTO t_fft VALUES (0.001,0.141120008);
+INSERT INTO t_fft VALUES (0.002,-0.279415498);
+INSERT INTO t_fft VALUES (0.003,0.412118485);
+INSERT INTO t_fft VALUES (0.004,-0.536572918);
+INSERT INTO t_fft VALUES (0.005,0.65028784);
+INSERT INTO t_fft VALUES (0.006,-0.750987247);
+INSERT INTO t_fft VALUES (0.007,0.836655639);
+INSERT INTO t_fft VALUES (0.008,-0.905578362);
+INSERT INTO t_fft VALUES (0.009,0.956375928);
+INSERT INTO t_fft SELECT a+0.01, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.02, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.04, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.08, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.16, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.32, 0.0 FROM t_fft;
+INSERT INTO t_fft SELECT a+0.64, 0.0 FROM t_fft;
+UPDATE t_fft SET b = sin(40 * 2 * 3.14 * a) + 0.5 * sin(90 * 2 * 3.14 * a);
+\timing
+\o /dev/null
+SELECT * FROM t_fft;
 -- Run the FFT and output amplitude vs. frequency
-SELECT pyFFT(a, b) OVER() FROM t;
+SELECT pyFFT(a, b) OVER() FROM t_fft;
+SELECT pyFFT(a, b) OVER() FROM t_fft;
+SELECT pyFFT(a, b) OVER() FROM t_fft;
 
 -- Step 4: Clean up
-DROP TABLE t;
-DROP LIBRARY TransformFunctions CASCADE;
+DROP TABLE t_fft;
+DROP LIBRARY fftTransformFunctions CASCADE;
